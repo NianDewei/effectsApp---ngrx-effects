855beef28f1611ee1bb62baa927efc8da6f59988
Rolando Oc <admi.jmb@gmail.com>
2023-03-26 08:22:58 -0500

    ⬆ | routes of our application

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NavbarComponent implements OnInit {
+  constructor(private _router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  // !methods
+  goUser(id: string): void {
+    if (!id) {
+      return;
+    }
+
+    this._router.navigate(['/user', id]);
   }
-
 }
