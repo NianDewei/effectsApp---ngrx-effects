c7a67250961ad10dbf7285f55285e6a27a016443
Rolando Oc <admi.jmb@gmail.com>
2023-04-02 08:34:12 -0500

    ⬆ | appReducer, storeModule and devTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { ListComponent } from './users/list/list.component';
+// !ngrx
 import { StoreModule } from '@ngrx/store';
+import { appReducers } from './store/app.reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +20,8 @@ import { StoreModule } from '@ngrx/store';
     AppRoutingModule,
     ListComponent,
     SharedModule,
-    StoreModule.forRoot({}, {}),
+    StoreModule.forRoot(appReducers),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent],
